f20a0ee6437a397c8a24f54c9a0e47cf9c2519a6
neamoth <neamoth15-8202@diu.edu.bd>
2025-05-01 02:44:54 +0600

    bg image issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar"
+import Hero from "./sections/Hero"
 
 function App() {
 
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Hero />
+    </>
   )
 }
 
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+    return (
+        <section className="container pt-[130px]">
+            <div className="w-[1440px] bg-amber-200 h-[100dvh]">
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Hero
